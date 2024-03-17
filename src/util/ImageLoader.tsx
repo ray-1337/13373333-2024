c6f9d796d9b909030432ce4ae44777767307ad37
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-18 00:39:36 +0100

    delete ImageLoader function

--- src/util/ImageLoader.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import type { ImageLoaderProps } from "next/image";
-
-export default function ImageLoader({ src }: ImageLoaderProps) {
-  return `https://itchi.2024.13373333.one${src}`;
-};
