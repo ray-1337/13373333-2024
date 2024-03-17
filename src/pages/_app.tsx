6f1ff99dce7f77fb1d43a1046191ee157e86696f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-17 13:28:57 +0100

    [draft] add hovering animation css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import "normalize.css";
 import '@mantine/core/styles.css';
 
+// css from mine
 import "@/styles/globals.css";
+import "@/styles/animations/hover.css";
 
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
