1777162125aefe35e66f96dbb880763d34db2a6e
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-17 13:31:32 +0100

    customize the way to import "@tabler/icons-react" through next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  transpilePackages: ['@tabler/icons-react'],
+
+  experimental: {
+    optimizePackageImports: ["@tabler/icons-react"]
+  }
 };
 
 export default nextConfig;
