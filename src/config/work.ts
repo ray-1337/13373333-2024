67f697fe11f1dc6c52168c76187aaca73acfe603
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-17 02:18:13 +0100

    added "WorkProp" type on work config

--- src/config/work.ts
+++ src/config/work.ts
@@ -1,2 +1,9 @@
 type AbandonType = "Discontinued" | "Under Construction";
 
+
+type WorkProp = Record<"name" | "description" | "url" | "imageURL", string> & {
+  abandonedType?: AbandonType
+  embedURL?: string;
+  type: WorkType;
+};
+
