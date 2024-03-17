cc7f0ba97039007b17329f9e046e8f648f9f2008
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-17 14:05:26 +0100

    add a function name to ImageLoader

--- src/util/ImageLoader.tsx
+++ src/util/ImageLoader.tsx
@@ -2,6 +2,6 @@
 
 import type { ImageLoaderProps } from "next/image";
 
-export default function ({ src }: ImageLoaderProps) {
+export default function ImageLoader({ src }: ImageLoaderProps) {
   return `https://itchi.2024.13373333.one${src}`;
 };
