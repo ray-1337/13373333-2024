8beead300083306770ece7df97d80deda9ffaf7c
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-18 00:38:25 +0100

    add csp to the header

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
+import { stripIndents } from 'common-tags';
+
 const isProd = process.env.NODE_ENV === "production";
 
 const cdnEndpoint = "itchi.2024.13373333.one";
 
+const cspHeader = stripIndents(`
+  default-src 'self';
+  script-src 'self' https://static.cloudflareinsights.com/beacon.min.js https://${cdnEndpoint} ${!isProd ? "'unsafe-eval'" : ""};
+  style-src 'self' https://fonts.googleapis.com/css2 https://${cdnEndpoint} 'unsafe-inline' ${!isProd ? "'unsafe-eval'" : ""};
+  img-src 'self' https://${cdnEndpoint} https://cdn.simpleicons.org https://i.ytimg.com/vi/;
+  font-src 'self' https://fonts.gstatic.com/s/ https://${cdnEndpoint};
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-src https://www.youtube-nocookie.com/;
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -21,6 +37,20 @@ const nextConfig = {
     loader: isProd ? "custom" : "default",
     domains: isProd ? [cdnEndpoint] : undefined,
     unoptimized: isProd ? undefined : true
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
   },
 };
 
