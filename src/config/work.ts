705d25c64c859fa5015a3d530ffbdbf9bc230787
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-03-17 13:23:43 +0100

    [draft] add WorkType type

--- src/config/work.ts
+++ src/config/work.ts
@@ -1,5 +1,6 @@
 type AbandonType = "Discontinued" | "Under Construction";
 
+type WorkType = "website";
 
 type WorkProp = Record<"name" | "description" | "url" | "imageURL", string> & {
   abandonedType?: AbandonType
