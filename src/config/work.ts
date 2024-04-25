89b32871dbff5e33edc3f17b0aece00daebce56c
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-04-25 22:04:06 +0800

    add credits on YouTube Discord Embed project

--- src/config/work.ts
+++ src/config/work.ts
@@ -89,7 +89,12 @@ export default [
     name: "YouTube Discord Embed",
     imageURL: "yde.webp",
     url: "https://github.com/ray-1337/youtube-discord-embed",
-    description: "Embed YouTube video on Discord, inspired by FixTweet."
+    credits: {
+      "Hosted on": {
+        "name": "Vercel",
+        "url": "https://vercel.com"
+      }
+    }
   },
   {
     name: "Discord Voice Message",
